ae8a89dca221504d50c04c50e4bda839108db8df
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2016-04-21 19:54:47 -0700

    Add variables for app/server setup

---
+++ server.js
@@ -0,0 +1,9 @@
+// set up 
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+var morgan         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+
